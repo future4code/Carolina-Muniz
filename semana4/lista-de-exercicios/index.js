6e20d39689d81ac06e33307fc1e3cdbb16de0e23
Carolina Souza <62221034+carolsouzamuniz@users.noreply.github.com>
2020-04-11 15:38:18 -0300

    resposta do projeto semana 4 - aula 14, exercicio 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,10 +1,27 @@
 /* Exercícios de leitura de código - aula 14
-1. a função faz a conversão do valor do dólar, solicitanto a cotação do dolar para o usuário,  
-   para responder quanto esse valor em dolar vale em real:
-   primeiro aparece uma janela solicitando ao usuário um valor da cotação do dólar
-   retornando o valor do dolar multiplicado pelo valor da cotação solicitado anteriormente
-   tendo a valor de conversorDeMoeda, é criado uma outra função que pega o valor que o usuário 
-   tem (meuDinheiro) para ser convertido em real.
+1. 
+a função faz a conversão do valor do dólar, solicitanto a cotação do dolar para o usuário,  
+para responder quanto esse valor em dolar vale em real:
+primeiro aparece uma janela solicitando ao usuário um valor da cotação do dólar
+retornando o valor do dolar multiplicado pelo valor da cotação solicitado anteriormente
+tendo a valor de conversorDeMoeda, é criado uma outra função que pega o valor que o usuário 
+tem (meuDinheiro) para ser convertido em real.
 */
+
+/* 2.
+a função chega várias condições.
+caso o usuário escolha a opção poupança, o valor do seu investimento vai ser mutiplicado por 1,03;
+opção Renda fixa,, o valor do investimento vai ser multiplicado por 1,05;
+opção CDB, o valor vai ser multiplicado por 1,06;
+opcao Açoes, o valor é multiplicado pro 1,1;
+e se não for nenhumas dessa opções parace um alerta dizendo que o investimento nao foi informado corretamente.
+a função retorna o valor após a multiplicao pelo percentual de cada investimento.
+criando uma nova variavel novoMontate, ele invoca a funçao investeDinheiro solicitando o investimento em açoes com o valor de 150
+e a variavel segundoMontate invoca a funcao investeDinheiro solicitando aplicacao no tesouro direto e o valor de 200, mas irá retornar investimento incorreto, pois 
+não há essa opçãod e investimento. 
+
+
+
+
 
  
