dc305354177bb450399e6614e3b6b395a350f4e8
Carolina Souza <62221034+carolsouzamuniz@users.noreply.github.com>
2020-04-11 14:31:27 -0300

    Resposta do desafio aula 14 - exercicio1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,10 @@
+/* Exercícios de leitura de código - aula 14
+1. a função faz a conversão do valor do dólar, solicitanto a cotação do dolar para o usuário,  
+   para responder quanto esse valor em dolar vale em real:
+   primeiro aparece uma janela solicitando ao usuário um valor da cotação do dólar
+   retornando o valor do dolar multiplicado pelo valor da cotação solicitado anteriormente
+   tendo a valor de conversorDeMoeda, é criado uma outra função que pega o valor que o usuário 
+   tem (meuDinheiro) para ser convertido em real.
+*/
+
+ 
