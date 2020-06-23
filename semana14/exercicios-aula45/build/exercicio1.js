31c663a3254201794f7168e26649b522f7d4b159
Carolina Souza <62221034+carolsouzamuniz@users.noreply.github.com>
2020-06-23 16:49:16 -0300

    resolução dos exercícios aula45

---
+++ semana14/exercicios-aula45/build/exercicio1.js
@@ -0,0 +1,7 @@
+"use strict";
+const name = process.argv[2];
+const age = Number(process.argv[3]);
+const newAge = age + 7;
+console.log(`Olá, ${name}! Você tem ${age} anos.`);
+console.log(`Olá, ${name}! Você tem ${age} anos. Em sete anos você terá ${newAge}.`);
+//# sourceMappingURL=exercicio1.js.map
